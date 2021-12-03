b62077d52ffafbccaacf79207a176e13a7f0e11d
Serhii Krimets <75704401+Sergio1004@users.noreply.github.com>
2021-12-03 14:09:51 +0200

    update package.jsons

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ export default function App() {
           console.log(error);
         });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, query]);
 
   const handleSearchSubmit = query => {
